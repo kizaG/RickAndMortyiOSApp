12a2c194d654d1a0d24d29d5a7666b01b05dfa4e
Nurgazy Shyrynkhan <nurgazy240598@gmail.com>
2023-08-29 13:56:19 +0600

    Wrap up search results

--- RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
@@ -23,6 +23,8 @@ final class RMSearchViewViewModel {
     
     private var noResultsHandler: (() -> Void)?
     
+    private var searchResultModel: Codable?
+    
     //MARK: - Init
     
     init(config: RMSearchViewController.Config) {
@@ -107,6 +109,7 @@ final class RMSearchViewViewModel {
         }
         
         if let results = resultsVM {
+            self.searchResultModel = model
             self.searchResultHandler?(results)
         } else {
             // fallback error
@@ -132,5 +135,12 @@ final class RMSearchViewViewModel {
         _ block: @escaping ((RMSearchInputViewViewModel.DynamicOption, String)) -> Void
     ) {
         self.optionMapUpdateBlock = block
+    }
+    
+    public func locationSearchResult(at index: Int) -> RMLocation? {
+        guard let searchModel = searchResultModel as? RMGetAllLocationsResponse else {
+            return nil
+        }
+        return searchModel.results[index]
     }
 }
